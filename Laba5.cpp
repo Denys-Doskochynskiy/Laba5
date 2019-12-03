d6e42090ac93784de2df405d25ac7d8214e4ebca
Denys-Doskochynskiy <55959253+Denys-Doskochynskiy@users.noreply.github.com>
2019-12-03 23:51:21 +0200

    Changed the code, renamed the variables, etc ...

--- Laba5.cpp
+++ Laba5.cpp
@@ -1,80 +1,80 @@
-﻿#include <iostream>
+#include <iostream>
 #include <math.h>
-
+#define SIZE 5
 class Vector {
 private:
-	int A[5];
+	int Array[SIZE];
 public:
 	friend void PrintArr(Vector* A, int n);
 	void setVectorel(int a) {
-		static int ptr = 0;
-		this->A[ptr] = a;
-		if (ptr == 4) ptr = -1;
-		ptr++;
+		static int pointer = 0;
+		this->Array[pointer] = a;
+		if (pointer == 4) pointer = -1;
+		pointer++;
 	}
-	int mul() {
+	int multipley() {
 		int s = 1;
-	for (int i = 0; i < 5; i++) {
-			s *= A[i];
+		for (int columns = 0; columns < 5; columns++) {
+			s *= Array[columns];
 		}
 		return s;
 	}
 	int* get_arr() {
-		return A;
+		return Array;
 	}
 	void SetArrEl(int c) {
 
-		std::cin >> this->A[c];
+		std::cin >> this->Array[c];
 
 	}
-	 void PrintArr(Vector* A, int n);
+	void PrintArr(Vector* A, int n);
 	void merge(int start, int mid, int end) {
-		int temp[5];
-		int i = start, j = mid + 1, k = 0;
+		int temp[SIZE];
+		int columns = start, j = mid + 1, k = 0;
 
-		while (i <= mid && j <= end) {
-			if (this->A[i] >= this->A[j]) {
-				temp[k] = this->A[i];
-				k += 1; i += 1;
+		while (columns <= mid && j <= end) {
+			if (this->Array[columns] >= this->Array[j]) {
+				temp[k] = this->Array[columns];
+				k += 1; columns += 1;
 			}
 			else {
-				temp[k] = this->A[j];
+				temp[k] = this->Array[j];
 				k += 1; j += 1;
 			}
 		}
 
-		while (i <= mid) {
-			temp[k] = this->A[i];
-			k += 1; i += 1;
+		while (columns <= mid) {
+			temp[k] = this->Array[columns];
+			k += 1; columns += 1;
 		}
 
 		while (j <= end) {
-			temp[k] = this->A[j];
+			temp[k] = this->Array[j];
 			k += 1; j += 1;
 		}
 
 		// copy temp to original interval
-		for (i = start; i <= end; i += 1) {
-			this->A[i] = temp[i - start];
+		for (columns = start; columns <= end; columns += 1) {
+			this->Array[columns] = temp[columns - start];
 		}
 	}
 
-	void mergeSort(int start, int end) {
+	void mergeSortСolumnsВescending(int start, int end) {
 
 		if (start < end) {
 			int mid = (start + end) / 2;
-			mergeSort(start, mid);
-			mergeSort(mid + 1, end);
+			mergeSortСolumnsВescending(start, mid);
+			mergeSortСolumnsВescending(mid + 1, end);
 			merge(start, mid, end);
 		}
 
 	}
 };
 
- void PrintArr(Vector* A, int n) {
-	 for (int i = 0; i > n; ++i)
+void PrintArr(Vector* Array, int n) {
+	for (int columns = 0; columns > n; ++columns)
 	{
-		std::cout << A->A[i];
+		std::cout << Array->Array[columns];
 	}
 	std::cout << std::endl;
 }
@@ -82,52 +82,45 @@ public:
 
 
 
-int main() {
-	Vector A[5];
-	for (int i = 0; i < 5; i++)
-		for (int j = 0; j < 5; j++) {
-			A[j].SetArrEl(i);
+int main( ) {
+	
+	Vector Array[SIZE];
+	for (int columns = 0; columns < 5; columns++)
+		for (int rows = 0; rows < 5; rows++) {
+			Array[rows].SetArrEl(columns);
 		}
 
-	Vector R;
-	Vector Fs;
-	
-	for (int i = 0; i < 5; i++)
-		A[i].mergeSort(0, 4);
-	for (int i = 0; i < 5; i++) {
-		for (int j = 0; j < 5; j++) {
-			std::cout << A[j].get_arr()[i] << "\t ";
-			R.setVectorel(A[j].get_arr()[i]);
+	Vector colums;
+	Vector arrayOfFunction;
+
+	for (int columns = 0; columns < 5; columns++)
+		Array[columns].mergeSortСolumnsВescending(0, 4);
+	for (int rows = 0; rows < SIZE; rows++) {
+		for (int columns = 0; columns < 5; columns++) {
+			std::cout << Array[columns].get_arr()[rows] << "\t ";
+			colums.setVectorel(Array[columns].get_arr()[rows]);
 		}
-		Fs.setVectorel(R.mul());
+		arrayOfFunction.setVectorel(colums.multipley);
 		std::cout << std::endl;
 	}
 
 
-	class Calc {
+	class calculationFunction {
 	public:
-		friend void Calcul(Calc Sum);
+		friend void Calculation(calculationFunction Sum);
 	};
-	void Culcul(Calc Sum); {//Ця функція просто лютий колхоз,сорі япотім її перероблю)))
-		int Sum = 0;
+	void Culculation(calculationFunction Sum); {
 	
-			std::cout << "f[i] :" << A[0].get_arr()[0] << std::endl;
-
-			std::cout << "f[i] :" << A[0].get_arr()[1] << std::endl;
-
-			std::cout << "f[i] :" << A[2].get_arr()[2] << std::endl;
-			std::cout << "f[i] :" << A[2].get_arr()[3] << std::endl;
-			std::cout << "f[i] :" << A[1].get_arr()[4] << std::endl;
-
-		
-		
-			Sum = A[0].get_arr()[0]+ A[0].get_arr()[1]+ A[2].get_arr()[2]+ A[2].get_arr()[3]+ A[1].get_arr()[4];
-		
+	int Result=0;
+		for (int k = 0; k < 5; k++) {
+			std::cout << "Max in colums "<<Array[k].get_arr()[0] << std::endl;
+			Result += Array[k].get_arr()[0];
+		}
 	
-		std::cout << "F(f[i]) :" << Sum << std::endl;
 
+std::cout << "the sum of the elements= " << Result << std::endl;
 		std::cout << std::endl;
-}
+	}
 
 
 	return 0;
